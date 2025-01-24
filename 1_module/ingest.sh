84eea062bd97cd5b3f868ebfaa1a111ed4be5242
Amr-Mohie-eldeen <amr.mohie@hotmail.com>
2025-01-24 23:51:48 +0000

    Created ingestion script container and postgres docker-compose

---
+++ 1_module/ingest.sh
@@ -0,0 +1,1 @@
+docker run --network '1_module_default' -it taxi_ingest:v01 --user='root' --password='root' --host='pgdatabase' --port='5432' --db='ny_taxi' --table_name='yellow_taxi_trips' --url='https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/yellow_tripdata_2021-01.csv.gz' 
